47f02bea9f882e100d268cabfdefb7358a1377c8
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-11 16:19:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ up = test.upload()
 down = (down/1024)/1024
 up = (up/1024)/1024
 
-print(f"Speed test by Mr-Cuda 🏌️‍♂️")
+print(f"Speed test by Mr-Cuda") # yes I want to take credits for 8 lines of code
 print(f"Download speed: {round(down, 2)} Mbps")
 print(f"Upload speed: {round(up, 2)} Mbps")
