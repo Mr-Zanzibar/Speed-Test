87af8b7cf0cc27f1463fa3eaa610d0931ec927f0
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-07-09 18:33:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ down = (down/1024)/1024
 up = (up/1024)/1024
 
 print(f' ')
-print(color.CYAN + f'Speed test by Mr-Cuda')
+print(color.CYAN + f'Speed test')
 print(color.CYAN + f'Download speed: {round(down, 2)} Mbps')
 print(color.CYAN + f'Upload speed: {round(up, 2)} Mbps')
 print(f' ')
